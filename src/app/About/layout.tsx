838bda401591f9a321c2f0c9fd7aac3f764ab569
Sourav <mehtas018@gmail.com>
2023-08-05 00:50:05 +0530

    Metadata, errors

--- src/app/About/layout.tsx
+++ src/app/About/layout.tsx
@@ -1,4 +1,10 @@
 import style from './styles.module.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'About',
+  description: "Watching Hitesh's live and learning basiscs of Next js",
+}
 
 export default function RootLayout({
     children, // will be a page or nested layout
--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 export default function About() {
+ // throw new Error("Test")
   return (
     <div className="flex flex-col items-center justify-between p-24">
         About Page
